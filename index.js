5c5d935b0b503734b5622d73c20f57dbdda11757
Sebastian <bitterulf@users.noreply.github.com>
2017-01-29 19:32:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const _ = require('lodash');
+const crawlJob = require('simple-spa-crawler').crawlJob;
+
+exports.register = function (server, options, next) {
+    var crawlCache = {};
+
+    crawlJob(server.info.uri, options.cronTime, options.wait, function (err, result) {
+        server.log(['spa', 'crawlJob', 'finished'], _.keys(result));
+        crawlCache = result;
+    });
+
+    server.ext('onRequest', function (request, reply) {
+        const fragment = request.query._escaped_fragment_;
+        if (fragment) {
+            const cachedHTML = crawlCache['/?' + fragment];
+            if (cachedHTML) {
+                server.log(['spa', 'cache', 'success'], fragment);
+                reply(cachedHTML);
+            } else {
+                server.log(['spa', 'cache', 'failed'], fragment);
+                return reply.continue();
+            }
+        } else {
+            return reply.continue();
+        }
+    });
+
+    next();
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
